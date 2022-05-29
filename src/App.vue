a3b8e15fb1ce0a1fee05ba40773fa5a2d3d4d2f5
terry3t <wangxi8206@sina.com>
2022-05-29 21:20:47 +0800

    add rsa tool

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
     </nav>
     <router-view/>
   </div>
 </template>
+
+<script>
+
+</script>
 
 <style>
 #app {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a class="twitter-share-button" target="blank"
+  href="https://twitter.com/intent/tweet?text=@nutbos create account: sdfsgsgf">
+Tweet</a>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import {getKeyPair} from "@/utils/rsa"
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  method() {
+
+  },
+  mounted() {
+    const a = getKeyPair('asdf')
+    console.log(235, a)
   }
 }
 </script>
+
