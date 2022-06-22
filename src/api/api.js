528e6d3b655d389fd5a00464ce53c055647e975e
wangxi <wangxi8206@sina.com>
2022-06-22 15:19:56 +0800

    add trans api

--- src/api/api.js
+++ src/api/api.js
@@ -10,5 +10,9 @@ export const getRegisterTicket = async (publicKey) =>
 export const getUsersPosts = async (username, pageSize, time, newPost) => 
     get(BACKEND_API_URL + '/twitter/getUsersPostsByTime', {username, pageSize, time, newPost})
 
+export const getUsersTransaction = async (twitterId, pageSize, time, newTrans) =>
+    get(BACKEND_API_URL + '/transaction/byTwitterId', { twitterId, pageSize, time, newTrans })
+
 export const getTwitterAccount = async (username) =>
-    getTwitterApi('https://api.twitter.com/twitter/2/users/by/username/' + username + '?user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,url,username,verified,withheld')
+getTwitterApi('https://api.twitter.com/twitter/2/users/by/username/' + username + '?user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,url,username,verified,withheld')
+
