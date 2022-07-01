2a1aae62c164ac5e78e9869561c885d9ca24ed7b
wangxi <wangxi8206@sina.com>
2022-07-01 10:21:21 +0800

    udpate config

--- vue.config.js
+++ vue.config.js
@@ -24,6 +24,14 @@ module.exports = defineConfig({
         chunks: "all",
       },
     },
+    performance: {
+      hints: 'warning',
+      maxEntrypointSize: 50000000,
+      maxAssetSize: 30000000,
+      assetFilter: function (assetFilename) {
+        return assetFilename.endsWith('.js')
+      }
+    },
   },
   devServer: {
     proxy: {
