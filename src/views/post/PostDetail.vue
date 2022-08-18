470c6db22fbcf84cdaa3f400469e3bcd0fbb7279
wangxi <wangxi8206@sina.com>
2022-08-18 11:28:14 +0800

    update active regex

--- src/views/post/PostDetail.vue
+++ src/views/post/PostDetail.vue
@@ -95,7 +95,7 @@ export default {
       let ac = content.split('#web3_ac')
       if (ac.length > 1) {
         ac = ac[1]
-        let infos = ac.replace('：', ':').replace('位置', '坐标');
+        let infos = ac.replace(/：/g, ':').replace(/位置/g, '坐标');
         try {
           const sponsor = infos.split('主办方:')[1].split('开始时间')[0]
           const sdate = infos.split('开始时间:')[1].split('结束时间')[0]
