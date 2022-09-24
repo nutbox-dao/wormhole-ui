77f7e3aa8f0f6287b89cc3b57fadbc657876b97d
terry3t <wangxi8206@sina.com>
2022-09-24 15:03:13 +0800

    fix issue

--- src/utils/tool.js
+++ src/utils/tool.js
@@ -27,6 +27,7 @@ export function copyAddress(address) {
 }
 
 export function formatEmojiText(str) {
+  if (!str) return ''
   const regStr = /[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF][\u200D|\uFE0F]|[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF]|[0-9|*|#]\uFE0F\u20E3|[0-9|#]\u20E3|[\u203C-\u3299]\uFE0F\u200D|[\u203C-\u3299]\uFE0F|[\u2122-\u2B55]|\u303D|[\A9|\AE]\u3030|\uA9|\uAE|\u3030/ig;
   // str = str.replace(regStr, (char) => {
   //   return `<span class="c-emoji">${char}</span>`;
