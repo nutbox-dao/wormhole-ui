38cf03ee5face3147b0da4420c3da8714f7e3a55
wangxi <wangxi8206@sina.com>
2022-11-04 10:28:21 +0800

    change login code view

---
+++ src/views/LoginCode.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        {{info}}
+    </div>
+</template>
+
+<script>
+import Cookie from 'vue-cookies'
+
+export default {
+  name: "LoginCode",
+  data() {
+    return {
+        info: ''
+    }
+  },
+  mounted () {
+    const code = this.$route.params.code;
+    console.log(325, this.$route.params);
+    if (code) {
+        this.info = 'Login Success'
+      Cookie.set('twitter-loginCode', code, '300s');
+      window.close();
+    };
+    this.info = 'Wrong login code'
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
