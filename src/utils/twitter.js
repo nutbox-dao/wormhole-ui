4f6ac0046d9702bb92c11fe218b75dc0940bd361
terry3t <wangxi8206@sina.com>
2022-11-10 23:35:24 +0800

    add twitter api

--- src/utils/twitter.js
+++ src/utils/twitter.js
@@ -4,17 +4,52 @@ import { refreshToken } from '@/utils/account'
 
 async function checkAccessToken() {
     let acc = store.getters.getAccountInfo;
-    const {accessToken, expiresAt} = acc;
-    if (expiresAt - new Date().getTime() < 600000) {
-        // refresh token 
-        await refreshToken();
-        console.log('expires');
-        acc = store.getters.getAccountInfo;
+    if (acc && acc.accessToken) {
+        const { expiresAt } = acc;
+        if (expiresAt - new Date().getTime() < 600000) {
+            // refresh token 
+            try {
+                await refreshToken();
+                acc = store.getters.getAccountInfo;
+            }catch(e) {
+                throw 'log out';
+            }
+        }
+        return acc.accessToken
+    }else {
+        // need auth again
+        throw 'log out';
     }
-    return acc.accessToken
 }
 
 export const getTweetById = async (tweetId) => {
+    console.log(1);
     const accessToken = await checkAccessToken();
-    const client = new Client()
+    console.log(2);
+    const client = new Client(accessToken);
+    console.log(3);
+    const tweet = await client.tweets.findTweetById('1590465906039091201');
+    const dd = await client.users.usersIdFollow('sdf', {
+        target_user_id: ''
+    })
+    // like a tweet
+    client.tweets.usersIdLike('adff', {
+        tweet_id: ''
+    })
+
+    // wheather user liked tweet
+    client.tweets.usersIdLikedTweets('2355', {
+
+    })
+
+    // wheather user follow someone
+    client.users.usersIdFollowing('23455', {
+        max_results: 23,
+        "user.fields": ['created_at'],
+        "tweet.fields": ['author_id'],
+        pagination_token: '',
+        
+    }, '45',
+    )
+    console.log(234, tweet, dd);
 }
