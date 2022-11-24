b258e841b48001bd8356960a8f24a3915f2bab2f
somiar <somiarzj@hotmail.com>
2022-11-24 10:27:05 +0800

    update tip

--- src/components/SpeakerTipModal.vue
+++ src/components/SpeakerTipModal.vue
@@ -26,50 +26,20 @@
       </div>
     </template>
     <template v-if="step===2">
-      <div class="flex justify-between items-center mt-2rem">
-        <span class="c-text-black">Chain</span>
-      </div>
-      <div class="w-full border-1 bg-black/40 border-1 border-color8B/30
-                  flex items-center justify-between
-                  light:bg-colorF2 light:border-colorE3 hover:border-primaryColor
-                  rounded-12px h-40px 2xl:h-2rem">
-        <el-select v-model="popUpData.chain" class="w-full" size="large">
-          <el-option label="Steem" value="steem"></el-option>
-          <div class="w-full h-1px bg-color8B/30 my-0.5rem"></div>
-          <div class="flex justify-between items-center px-1.5rem ">
-            <span class="c-text-black">Other</span>
-            <span class="text-color8B">Only available for registered Wormhole3 users</span>
-          </div>
-          <el-option
-              v-for="item in chainOptions"
-              :disabled="false"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-          />
-        </el-select>
-      </div>
-      <div class="flex justify-between items-center mt-2rem">
-        <span class="c-text-black">Amount</span>
-      </div>
-      <div class="w-full border-1 bg-black/40 border-1 border-color8B/30
-                  flex items-center justify-between
-                  light:bg-colorF2 light:border-colorE3 hover:border-primaryColor
-                  rounded-12px h-40px 2xl:h-2rem">
-        <input class="bg-transparent h-full w-full px-0.5rem flex-1"
-               v-model="popUpData.amount"
-               type="number"
-               placeholder="">
-        <div class="h-20px 2xl:h-1.5rem w-1px bg-color7D"></div>
-        <el-select v-model="popUpData.token" class="w-1/3" size="large">
-          <el-option
-              v-for="item in tokenOptions"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-          />
-        </el-select>
-      </div>
+      <AssetsOptions :chain="form.chain"
+                     :address="form.address"
+                     :token="form.token"
+                     :showsteem="false"
+                     @chainChange="selectChain"
+                     @tokenChagne="selectToken"
+                     @addressChange="selectAddress"
+                     @balanceChange="selectBalance">
+        <template #amount>
+          <input class="bg-transparent h-full w-full px-0.5rem"
+                 v-model="form.amount"
+                 type="number" :placeholder="$t('curation.inputRewardsAmount')">
+        </template>
+      </AssetsOptions>
       <div class="flex items-center justify-center gap-x-1rem">
         <button class="gradient-btn w-16rem h-3.6rem rounded-full mt-3rem" @click="step=1">
           back
@@ -83,6 +53,7 @@
 </template>
 
 <script>
+import AssetsOptions from "@/components/AssetsOptions";
 export default {
   name: "SpeakerTipModal",
   props: {
@@ -99,17 +70,12 @@ export default {
       speakers:[],
       popUpData: {
         chain: '',
-        amount: 0,
-        token: 'steem'
+        address: '',
+        token: '',
+        amount: 0
       },
-      chainOptions: [
-          {label: 'Ethereum', value: 'ethereum'},
-          {label: 'BSC', value: 'bsc'},
-          {label: 'Polygon', value: 'polygon'},
-      ],
-      tokenOptions: [
-          {label: 'Steem', value: 'steem'}
-      ]
+      selectedToken: {},
+      selectedBalance: ''
     }
   },
   methods: {
