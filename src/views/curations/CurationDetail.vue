271266b2bf0cb326dc24dbbaa6d948eed6645fd8
wangxi <wangxi8206@sina.com>
2022-12-01 18:28:49 +0800

    fix issue

--- src/views/curations/CurationDetail.vue
+++ src/views/curations/CurationDetail.vue
@@ -11,16 +11,16 @@
               <div class="flex items-start mb-1rem">
                 <img class="w-2.6rem md:h-2.6rem md:w-50px md:h-50px md:min-h-50px md:mr-30px mr-0.8rem rounded-full cursor-pointer"
                      @error="replaceEmptyImg"
-                     @click="gotoUserPage(detailCuration && detailCuration.twitterUsername)"
-                     :src="detailCuration.profileImg && detailCuration.profileImg.replace('normal', '200x200')" alt="">
-                <div class="flex flex-col justify-center items-start cursor-pointer" @click="gotoUserPage(detailCuration && detailCuration.twitterUsername)">
+                     @click="gotoUserPage(detailCuration && detailCuration.creatorTwitterUsername)"
+                     :src="detailCuration.creatorProfileImg && detailCuration.creatorProfileImg.replace('normal', '200x200')" alt="">
+                <div class="flex flex-col justify-center items-start cursor-pointer" @click="gotoUserPage(detailCuration && detailCuration.creatorTwitterUsername)">
                   <a class="c-text-black text-16px 2xl:text-0.8rem leading-24px 2xl:leading-1rem mr-0.8rem">{{detailCuration && detailCuration.creatorTwitterName}}</a>
                   <span class="text-15px 2xl:text-0.75rem text-color8B light:text-color7D leading-22px 2xl:leading-1.1rem">@{{detailCuration && detailCuration.creatorTwitterUsername}}</span>
                 </div>
                 <ContentTags :is-quote="isQuote" :is-reply="isReply" :content-type="contentType"/>
               </div>
               <template v-if="contentType==='tweet'">
-                <Blog :post="detailCuration" class="border-1 border-color8B/30 light:border-colorD6 rounded-12px">
+                <Blog :post="detailCuration" @click="gotoTweet" class="border-1 border-color8B/30 light:border-colorD6 rounded-12px">
                   <template #bottom-btn-bar><div></div></template>
                 </Blog>
               </template>
@@ -495,6 +495,9 @@ export default {
     formatEmojiText,
     onCopy,
     formatAmount,
+    gotoTweet() {
+      window.open('https://twitter.com/' + this.detailCuration.username + '/status/' + this.detailCuration.tweetId)
+    },  
     createTime(p) {
       if (!this.detailCuration || !this.detailCuration.createdTime || !this.detailCuration.endtime) return '';
       return parseTimestamp(p.createAt)
