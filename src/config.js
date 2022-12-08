e989f5fad37d042a79ab680adfd1e7e095991742
wangxi <wangxi8206@sina.com>
2022-12-08 18:26:24 +0800

    udpate to product env

--- src/config.js
+++ src/config.js
@@ -7,12 +7,12 @@ export const TWITTER_MONITOR_ACCOUNT = '@NutboxDao'
 export const TWITTER_MONITOR_RULE = "@wormhole_3"
 export const TWITTER_POST_TAG = "%23iweb3"
 
-// export const BACKEND_API_URL = 'https://alpha-api.wormhole3.io'
+export const BACKEND_API_URL = 'https://alpha-api.wormhole3.io'
 // export const BACKEND_API_URL = 'http://localhost:3100'
-export const BACKEND_API_URL = 'https://api-test.web3id.pro'
+// export const BACKEND_API_URL = 'https://api-test.web3id.pro'
 
-// export const CURATION_SHORT_URL = 'https://wh3.io/'
-export const CURATION_SHORT_URL = 'https://test.wormhole3.io/#/curation-detail/'
+export const CURATION_SHORT_URL = 'https://wh3.io/'
+// export const CURATION_SHORT_URL = 'https://test.wormhole3.io/#/curation-detail/'
 
 export const SignUpMessage = JSON.stringify({
     project: 'wormhole3',
