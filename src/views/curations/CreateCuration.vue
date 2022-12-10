2a553e5f8863e0d39923fbc2fe93ae8748ddbeed
wangxi <wangxi8206@sina.com>
2022-12-10 12:11:21 +0800

    del log

--- src/views/curations/CreateCuration.vue
+++ src/views/curations/CreateCuration.vue
@@ -433,15 +433,11 @@ import Steps from "@/components/Steps";
 import SendTokenTip from "@/components/SendTokenTip";
 import TwitterCompleteTip from "@/components/TwitterCompleteTip";
 import {markRaw, ref} from "vue";
-import { postErr } from '@/api/api'
 import { getTweetById, getSpaceById, getUserInfoByUserId } from '@/utils/twitter'
 import { getSpaceIdFromUrls } from '@/utils/twitter-tool'
 import { mapGetters, mapState } from 'vuex'
 import { notify, showError } from "@/utils/notify";
-import { setupNetwork, chainChanged, lockStatusChanged } from '@/utils/web3/web3'
-import { getTokenInfo, getERC20TokenBalance } from '@/utils/asset'
-import { accountChanged, getAccounts, updateAllUsersByPolling } from '@/utils/web3/account'
-import { CHAIN_ID, ERC20List, CURATION_SHORT_URL, EVM_CHAINS, TokenIcon } from "@/config";
+import { CURATION_SHORT_URL, EVM_CHAINS, TokenIcon } from "@/config";
 import { ethers } from 'ethers'
 import { sleep, formatAmount } from '@/utils/helper'
 import { randomCurationId, creteNewCuration, newCurationWithTweet, newCuration } from '@/utils/curation'
@@ -771,7 +767,6 @@ export default {
     },
     checkCreateData() {
       if (!this.form.description ||this.form.description.length === 0) {
-        console.log(1);
         notify({message: this.$t('tips.missingInput'), duration: 5000, type: 'error'})
         return false;
       }
@@ -782,22 +777,18 @@ export default {
         }
       }else {
         if (!this.form.author) {
-        console.log(2);
           notify({message: this.$t('tips.missingInput'), duration: 5000, type: 'error'})
           return false
         }
         if (!this.form.link || !this.form.endtime) {
-        console.log(3);
           notify({message: this.$t('tips.missingInput'), duration: 5000, type: 'error'})
           return false
         }
         if (this.form.category === 'tweet' && !this.form.postData?.postId) {
-        console.log(4);
           notify({message: this.$t('tips.missingInput'), duration: 5000, type: 'error'})
           return false
         }
         if (this.form.category === 'space' && !this.form.space?.spaceId) {
-        console.log(5);
           notify({message: this.$t('tips.missingInput'), duration: 5000, type: 'error'})
           return false
         }
@@ -856,6 +847,7 @@ export default {
       return true
     },
     async onSubmit() {
+      if (!this.checkLogin()) return
       if(!this.checkRewardData()) return;
       try{
         this.loading = true
@@ -871,10 +863,10 @@ export default {
       } finally {
         this.loading = false
       }
-
     },
     // created curation on chain
     async createCuration() {
+      if (!this.checkLogin()) return
       try{
         this.loading = true
         const curation = {
