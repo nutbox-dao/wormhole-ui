95e9dd2482d118f6594ad203ae5658398ebde8e8
terry3t <wangxi8206@sina.com>
2023-01-07 21:12:23 +0800

    update space issue

--- src/components/Space.vue
+++ src/components/Space.vue
@@ -2,20 +2,20 @@
   <div class="p-17px 2xl:p-1rem sm:rounded-1rem flex flex-col justify-between">
     <div class="flex justify-between items-center text-14px 2xl:text-0.8rem">
       <div class="flex items-center">
-        <img v-if="space.authorProfileImg || space.creatorProfileImg"
+        <img v-if="space.authorProfileImg || space.profileImg"
              class="w-30px h-30px xl:w-1.5rem xl:h-1.5rem rounded-full"
-             :src="(space.authorProfileImg ?? space.creatorProfileImg).replace('normal', '200x200')" alt="">
+             :src="(space.authorProfileImg ?? space.profileImg).replace('normal', '200x200')" alt="">
         <img v-else class="w-30px h-30px xl:w-1.5rem xl:h-1.5rem rounded-full opacity-50"
              src="~@/assets/icon-default-avatar.svg" alt="">
         <div class="flex-1 flex flex-col items-start cursor-pointer" @click.stop="gotoUserPage()">
           <div class="flex items-center flex-wrap">
             <a class="c-text-black text-left mr-3 ml-3 text-16px leading-18px 2xl:text-1rem 2xl:leading-1.5rem text-white">
-              @{{ space.authorUsername ?? space.creatorTwitterUsername }}</a>
+              @{{ space.authorUsername ?? space.username }}</a>
           </div>
         </div>
       </div>
     </div>
-    <div class="text-left c-text-black text-16px 2xl:text-1.2rem text-white">{{ space.spaceTitle }}</div>
+    <div class="text-left c-text-black text-16px 2xl:text-1.2rem text-white">{{ space.spaceTitle ?? space.authorName ?? space.name }}</div>
     <button class="bg-white h-30px 2xl:1.5rem w-full rounded-full font-bold flex justify-center items-center"
       @click.stop="gotoSpace">
      <img v-if="space.spaceState === 2" class="w-10x mr-5px" src="~@/assets/icon-listen.svg" alt="">
