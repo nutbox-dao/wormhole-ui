e20e6e33102e0e282f68b7430bb0674f8d772725
somiar <somiarzj@hotmail.com>
2023-01-10 13:23:14 +0800

    update ui

--- src/views/red-envelope/GiveCardModal.vue
+++ src/views/red-envelope/GiveCardModal.vue
@@ -140,7 +140,7 @@ export default {
   },
   data() {
     return {
-      step: 1,
+      step: 0,
       cards: [
         {label: 'SBF Rabbit', balance: 0, img: card0},
         {label: 'MashiMaro', balance: 0, img: card1},
