562d65f101eca4450a7922346ff49c11ae9de19b
wangxi <wangxi8206@sina.com>
2023-01-18 15:08:11 +0800

    udpate

--- src/views/curations/CreateCuration.vue
+++ src/views/curations/CreateCuration.vue
@@ -901,7 +901,7 @@ export default {
         const startTime = this.form.space.scheduledStart;
         date = new Date(startTime).getTime();
       }
-      return time.getTime() + 86400000 < date || time.getTime() >date + 86400000*7
+      return time.getTime() + 86400000 < date || time.getTime() >date + 86400000*21
     },
     checkCreateData() {
       if (!this.form.description ||this.form.description.length === 0) {
