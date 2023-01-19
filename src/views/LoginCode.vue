0786deb3363c1c01f6674e53ef7cd81bad967d34
terry3t <wangxi8206@sina.com>
2023-01-19 22:48:28 +0800

    update login

--- src/views/LoginCode.vue
+++ src/views/LoginCode.vue
@@ -6,6 +6,7 @@
 
 <script>
 import Cookie from 'vue-cookies'
+import { twitterLogin, twitterAuth } from '@/api/api'
 
 export default {
   name: "LoginCode",
@@ -14,17 +15,28 @@ export default {
         info: ''
     }
   },
-  mounted () {
-    const code = this.$route.params.code;
+  async mounted () {
+    const code = this.$route.query.code;
+    window.close();
     
     if (code) {
-        this.info = 'Login Success'
-        // Cookie.set('twitter-loginCode', code, '300s');
-        window.close();
+        // this.info = 'Login Success'
+        // const userInfo = await twitterLogin(code)
+        // console.log(35, userInfo)
+        // if (userInfo.code === 0) {
+        //   // not registry
+        //   // store auth info
+        //   console.log('not register')
+        //   Cookie.set('account-auth-info', JSON.stringify(userInfo.account), '180s')
+        //   return;
+        // }else if (userInfo.code === 3) { // log in
+        //   this.$store.commit('saveAccountInfo', userInfo.account)
+        //   this.$router.replace('/')
+        //   return;
+        // }
     }else {
         // login fail
         // Cookie.set('twitter-loginCode', 'fail', '300s');
-        window.close();
     };
     this.info = 'Login Success, please close this window'
   },
