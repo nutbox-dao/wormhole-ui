3240d6c8452e3b4ab75e59262aa223f82f697144
somiar <somiarzj@hotmail.com>
2023-03-19 16:33:22 +0800

    fix ui

--- src/views/word-cloud/Index.vue
+++ src/views/word-cloud/Index.vue
@@ -37,30 +37,7 @@
             <div class="relative" id="share-img">
               <img class="w-700px min-w-500px" src="~@/assets/word-cloud-share.png" alt="">
               <div class="absolute top-2/5 h-1/3 w-full px-1/10">
-                <div class="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 h-3/5">
-                  <svg width="100%" height="100%" viewBox="0 0 210 210" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                    <g id="页面-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" opacity="0.400000006">
-                      <g id="word-cloud-app备份-13" transform="translate(-270.000000, -796.000000)" :stroke="effectiveWordColor" stroke-width="5">
-                        <g id="p1" transform="translate(-170.000000, -510.000000)">
-                          <g id="编组-7" transform="translate(245.000000, 896.000000)">
-                            <g id="编组-4" transform="translate(35.000000, 205.000000)">
-                              <g id="500x300" transform="translate(15.000000, 147.000000)">
-                                <path d="M340,240.5 C343.45178,240.5 346.57678,241.89911 348.838835,244.161165 C351.10089,246.42322 352.5,249.54822 352.5,253 C352.5,256.45178 351.10089,259.57678 348.838835,261.838835 C346.57678,264.10089 343.45178,265.5 340,265.5 C336.54822,265.5 333.42322,264.10089 331.161165,261.838835 C328.89911,259.57678 327.5,256.45178 327.5,253 C327.5,249.54822 328.89911,246.42322 331.161165,244.161165 C333.42322,241.89911 336.54822,240.5 340,240.5 Z M250,66.5 C276.647739,66.5 300.772739,77.3011304 318.235804,94.7641956 C335.69887,112.227261 346.5,136.352261 346.5,163 C346.5,189.647739 335.69887,213.772739 318.235804,231.235804 C300.772739,248.69887 276.647739,259.5 250,259.5 C223.352261,259.5 199.227261,248.69887 181.764196,231.235804 C164.30113,213.772739 153.5,189.647739 153.5,163 C153.5,136.352261 164.30113,112.227261 181.764196,94.7641956 C199.227261,77.3011304 223.352261,66.5 250,66.5 Z M250,131.5 C241.301515,131.5 233.426515,135.025758 227.726136,140.726136 C222.025758,146.426515 218.5,154.301515 218.5,163 C218.5,171.698485 222.025758,179.573485 227.726136,185.273864 C233.426515,190.974242 241.301515,194.5 250,194.5 C258.698485,194.5 266.573485,190.974242 272.273864,185.273864 C277.974242,179.573485 281.5,171.698485 281.5,163 C281.5,154.301515 277.974242,146.426515 272.273864,140.726136 C266.685257,135.13753 259.006472,131.639101 250.510716,131.504056 Z M160,60.5 C163.45178,60.5 166.57678,61.8991102 168.838835,64.1611652 C171.10089,66.4232203 172.5,69.5482203 172.5,73 C172.5,76.4517797 171.10089,79.5767797 168.838835,81.8388348 C166.57678,84.1008898 163.45178,85.5 160,85.5 C156.54822,85.5 153.42322,84.1008898 151.161165,81.8388348 C148.89911,79.5767797 147.5,76.4517797 147.5,73 C147.5,69.5482203 148.89911,66.4232203 151.161165,64.1611652 C153.42322,61.8991102 156.54822,60.5 160,60.5 Z" id="形状结合"></path>
-                              </g>
-                            </g>
-                          </g>
-                        </g>
-                      </g>
-                    </g>
-                  </svg>
-                </div>
-                <vue-word-cloud
-                    :words="words"
-                    :color="getWordColor"
-                    :spacing="1/2"
-                    font-family="Roboto"
-                />
-<!--                <img src="~@/assets/word-cloud-demo.png" alt="">-->
+                <img class="h-full mx-auto" :src="imgUrl" alt="">
               </div>
               <div class="absolute bottom-1/12 w-full flex items-center justify-between pl-1/10 pr-1/10">
                 <div class="text-14px leading-20px text-color33/10 whitespace-pre-line mr-1/20">
@@ -73,37 +50,14 @@
             </div>
           </div>
           <div class="relative">
-            <img class="w-350px" :src="imgUrl" alt="">
+            <img class="w-350px" src="~@/assets/word-cloud-card.png" alt="">
             <div class="text-24px absolute top-64px pl-20px">{{$t('wordCloud.title')}}</div>
             <div class="absolute top-130px pl-20px pr-60px text-12px text-color33/10
                         whitespace-pre-line leading-16px transform scale-90">
               {{$t('wordCloud.wordDesc')}}
             </div>
             <div class="absolute top-210px h-1/3 w-full px-40px">
-              <div class="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 h-3/5">
-                <svg width="100%" height="100%" viewBox="0 0 210 210" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                  <g id="页面-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" opacity="0.400000006">
-                    <g id="word-cloud-app备份-13" transform="translate(-270.000000, -796.000000)" :stroke="effectiveWordColor" stroke-width="5">
-                      <g id="p1" transform="translate(-170.000000, -510.000000)">
-                        <g id="编组-7" transform="translate(245.000000, 896.000000)">
-                          <g id="编组-4" transform="translate(35.000000, 205.000000)">
-                            <g id="500x300" transform="translate(15.000000, 147.000000)">
-                              <path d="M340,240.5 C343.45178,240.5 346.57678,241.89911 348.838835,244.161165 C351.10089,246.42322 352.5,249.54822 352.5,253 C352.5,256.45178 351.10089,259.57678 348.838835,261.838835 C346.57678,264.10089 343.45178,265.5 340,265.5 C336.54822,265.5 333.42322,264.10089 331.161165,261.838835 C328.89911,259.57678 327.5,256.45178 327.5,253 C327.5,249.54822 328.89911,246.42322 331.161165,244.161165 C333.42322,241.89911 336.54822,240.5 340,240.5 Z M250,66.5 C276.647739,66.5 300.772739,77.3011304 318.235804,94.7641956 C335.69887,112.227261 346.5,136.352261 346.5,163 C346.5,189.647739 335.69887,213.772739 318.235804,231.235804 C300.772739,248.69887 276.647739,259.5 250,259.5 C223.352261,259.5 199.227261,248.69887 181.764196,231.235804 C164.30113,213.772739 153.5,189.647739 153.5,163 C153.5,136.352261 164.30113,112.227261 181.764196,94.7641956 C199.227261,77.3011304 223.352261,66.5 250,66.5 Z M250,131.5 C241.301515,131.5 233.426515,135.025758 227.726136,140.726136 C222.025758,146.426515 218.5,154.301515 218.5,163 C218.5,171.698485 222.025758,179.573485 227.726136,185.273864 C233.426515,190.974242 241.301515,194.5 250,194.5 C258.698485,194.5 266.573485,190.974242 272.273864,185.273864 C277.974242,179.573485 281.5,171.698485 281.5,163 C281.5,154.301515 277.974242,146.426515 272.273864,140.726136 C266.685257,135.13753 259.006472,131.639101 250.510716,131.504056 Z M160,60.5 C163.45178,60.5 166.57678,61.8991102 168.838835,64.1611652 C171.10089,66.4232203 172.5,69.5482203 172.5,73 C172.5,76.4517797 171.10089,79.5767797 168.838835,81.8388348 C166.57678,84.1008898 163.45178,85.5 160,85.5 C156.54822,85.5 153.42322,84.1008898 151.161165,81.8388348 C148.89911,79.5767797 147.5,76.4517797 147.5,73 C147.5,69.5482203 148.89911,66.4232203 151.161165,64.1611652 C153.42322,61.8991102 156.54822,60.5 160,60.5 Z" id="形状结合"></path>
-                            </g>
-                          </g>
-                        </g>
-                      </g>
-                    </g>
-                  </g>
-                </svg>
-              </div>
-              <vue-word-cloud
-                  :words="words"
-                  :color="getWordColor"
-                  :spacing="1/2"
-                  font-family="Roboto"
-              />
-<!--              <img src="~@/assets/word-cloud-demo.png" alt="">-->
+              <img :src="imgUrl" alt="">
             </div>
             <button class="absolute bottom-30px right-40px" @click="onDownload">
               <img class="h-30px w-30px" src="~@/assets/word-cloud-download.svg" alt="">
@@ -142,6 +96,7 @@ import cardBg from '@/assets/word-cloud-card.png'
 import VueWordCloud from "vuewordcloud";
 import QrcodeVue from 'qrcode.vue'
 import domtoimage from 'dom-to-image';
+import demoImg from '@/assets/word-cloud-demo.png'
 
 export default {
   name: "Index",
@@ -149,7 +104,7 @@ export default {
   data() {
     return {
       loading: false,
-      imgUrl: null,
+      imgUrl: demoImg,
       wallet: null,
       pair: null,
       pendingAccount: null,
@@ -328,15 +283,15 @@ export default {
     }
   },
   async mounted () {
-    await this.$router.isReady();
-    const referee = this.$route.query.referee;
-    if (referee) {
-      this.$store.commit('saveReferee', referee);
-    }
-    if (this.getAccountInfo) {
-      this.imgUrl = this.getAccountInfo.wordCloudUrl
-    }
-    this.setWordsStyle()
+    // await this.$router.isReady();
+    // const referee = this.$route.query.referee;
+    // if (referee) {
+    //   this.$store.commit('saveReferee', referee);
+    // }
+    // if (this.getAccountInfo) {
+    //   this.imgUrl = this.getAccountInfo.wordCloudUrl
+    // }
+    // this.setWordsStyle()
   },
 }
 </script>
