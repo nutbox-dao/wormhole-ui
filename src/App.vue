aed783917f8cee0941486941d56ca2a3406c7581
wangxi <wangxi8206@sina.com>
2023-04-12 18:42:48 +0800

    update rc circle color

--- src/App.vue
+++ src/App.vue
@@ -85,20 +85,18 @@
                      :src="profileImg" @error="replaceEmptyImg" alt="">
                 <el-progress v-if="$refs.appRef" class="absolute -top-1px -left-1px" type="circle"
                              :width="$refs.appRef.clientWidth>1280?42:37"
-                             :color="isDark?'#86e3ce':'#428cd4'"
+                             :color="isDark?'#8208e9':'#02f900'"
                              :show-text="false"
                              :stroke-width="3"
                              :percentage="rc/MAX_RC * 100">
                 </el-progress>
-                <!-- 白模式颜色为：#428cd4 -->
                 <el-progress v-if="$refs.appRef" class="absolute -top-2px -left-2px" type="circle"
                              :width="$refs.appRef.clientWidth>1280?44:39"
-                             :color="isDark?'#ffdd94':'#004e9a'"
+                             :color="isDark?'#00fa97':'#8644ef'"
                              :show-text="false"
                              :stroke-width="2"
                              :percentage="vp/MAX_VP * 100">
                 </el-progress>
-                <!-- 白模式颜色为#004e9a -->
               </router-link>
               <router-link :to="`/wallet/@${getAccountInfo.twitterUsername}/wallet`">
                 <i class="w-20px h-20px xl:h-1.4rem xl:w-1.4rem mr-0.4rem icon-wallet black-filter"></i>
