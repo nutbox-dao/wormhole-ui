cb88af60ee56b95c771222a8d5b32deccfd23472
wangxi <wangxi8206@sina.com>
2023-06-01 15:22:03 +0800

    fix

--- src/views/user/RewardView.vue
+++ src/views/user/RewardView.vue
@@ -61,11 +61,6 @@
                     <div v-else class="py-15px text-center text-color7D text-14px">
                       {{$t('walletView.claimedAllRewards')}}
                     </div>
-                    <div class="flex justify-center items-center">
-                      <button @click="changeCollapse(reward.token)">
-                        <img class="w-12px transform rotate-180" src="~@/assets/icon-select-arrow.svg" alt="">
-                      </button>
-                    </div>
                   </div>
                 </el-collapse-item>
               </el-collapse>
@@ -204,11 +199,6 @@ export default {
     }
   },
   methods: {
-    changeCollapse(token) {
-      this.collapseNames = this.collapseNames.filter(item => {
-        return item!==token
-      })
-    },
     formatAddress,
     formatAmount,
     selectTab(index) {
