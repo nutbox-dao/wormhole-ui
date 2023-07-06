383759a59dce3d1fd40e1ff31c6abf253ead6d67
terry3t <wangxi8206@sina.com>
2023-07-06 21:44:48 +0800

    fix unlike tip

--- src/components/PostButtonGroup.vue
+++ src/components/PostButtonGroup.vue
@@ -701,6 +701,10 @@ export default {
           notify({message: this.$t('tips.insufficientRC'), type: 'info', duration: 5000})
           return;
         }
+        if (e === errCode.INSUFFICIENT_VP) {
+          notify({message: this.$t('tips.insufficientVP'), type: 'info', duration: 5000});
+          return;
+        }
         if (e === errCode.TWITTER_ERROR) {
           notify({message: this.$t('tips.likeTooFrequent'), type: 'info', duration: 5000});
           return;
