6203bd3a01a4b76b24ccda2e4b64fadae22f5136
wangxi <wangxi8206@sina.com>
2023-08-23 09:27:42 +0800

    update font size

--- src/components/LanguageChangeButton.vue
+++ src/components/LanguageChangeButton.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
         <button
-         class="h-26px bg-color8B light:bg-blueDark rounded-full flex items-center justify-between"
+         class="h-24px bg-color8B light:bg-blueDark rounded-full flex items-center justify-between"
           @click="changeLang">
-          <span class="text-18px h-26px px-8px flex items-center" :class="lang === 'en' ? 'bg-color62 rounded-full' : ''">
+          <span class="text-14px h-24px px-8px flex items-center" :class="lang === 'en' ? 'bg-color62 rounded-full' : ''">
             EN
           </span>
-          <span class="text-18px h-26px px-8px flex items-center" :class="lang === 'zh' ? 'bg-color62 rounded-full' : ''">
+          <span class="text-14px h-24px px-8px flex items-center" :class="lang === 'zh' ? 'bg-color62 rounded-full' : ''">
             CH
           </span>
         </button>
