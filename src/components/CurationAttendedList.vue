889a7605e12e912a36b16804b29e479338c82781
wangxi <wangxi8206@sina.com>
2023-09-06 11:08:48 +0800

    add reply as curation op

--- src/components/CurationAttendedList.vue
+++ src/components/CurationAttendedList.vue
@@ -53,6 +53,12 @@
               <el-tooltip v-if="(record.taskRecord & 4) === 4" placement="top-start">
                 <i @click.stop class="w-16px h-16px min-w-16px btn-icon-like-active"></i>
                 <template #content>
+                  <div class="text-white light:text-black">+3</div>
+                </template>
+              </el-tooltip>
+              <el-tooltip v-if="(record.taskRecord & 2) === 2" placement="top-start">
+                <i @click.stop class="w-16px h-16px min-w-16px btn-icon-reply-active"></i>
+                <template #content>
                   <div class="text-white light:text-black">+5</div>
                 </template>
               </el-tooltip>
