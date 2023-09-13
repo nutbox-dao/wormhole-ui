ab4849c292d705b3a52c2b49c589c9db038ebbae
wangxi <wangxi8206@sina.com>
2023-09-13 16:09:31 +0800

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.k708oquhaao"
+    "revision": "0.6kercsfhgho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
